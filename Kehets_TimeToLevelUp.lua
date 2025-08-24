1e448099ecc9bcd3f77a5ba54ce7b6fe8061e2a1
Kehet <Kehet@users.noreply.github.com>
2025-08-24 12:02:39 +0300

    Initial Commit

---
+++ Kehets_TimeToLevelUp.lua
@@ -0,0 +1,5 @@
+local TimeToLevelUp = LibStub("AceAddon-3.0"):NewAddon("Kehet's TimeToLevelUp", "AceEvent-3.0", "AceConsole-3.0")
+
+function TimeToLevelUp:OnEnable()
+    self:Print("Enabled")
+end
